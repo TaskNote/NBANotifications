c1e9851a434e4d66045b3134bbbc8899a4ac751f
Jayon Huh <jayonhuh@jayons-mbp.lan>
2019-12-09 21:02:39 -0800

    new push brah added more methods

--- src/main/scala/org/millsmont/nbaNotifications/models/GameData.scala
+++ src/main/scala/org/millsmont/nbaNotifications/models/GameData.scala
@@ -3,7 +3,7 @@ package org.millsmont.nbaNotifications.models
 import scala.math.abs
 
 // TODO consider making gameid a string
-case class LiveScoreReader(gameId: Int, homePoints: Int, awayPoints: Int) {
+case class LiveScoreReader(gameId: Int, period: Int, homePoints: Int, awayPoints: Int, awayTeam: String, homeTeam: String) {
 
   val diff = abs(homePoints - awayPoints)
 
